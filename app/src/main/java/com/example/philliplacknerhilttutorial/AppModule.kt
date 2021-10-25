689f3411b8c305e8ab68bdbecf27490ccf6efadd
Brian Donnoe <dunow33@hotmail.com>
2021-10-25 19:36:09 -0400

    first commit

---
+++ app/src/main/java/com/example/philliplacknerhilttutorial/AppModule.kt
@@ -0,0 +1,18 @@
+package com.example.philliplacknerhilttutorial
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    @Named("String1")
+    fun provideTestString1() = "This is a string we will inject"
+}
---
+++ app/src/main/java/com/example/philliplacknerhilttutorial/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.philliplacknerhilttutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    @Inject
+    @Named("String1")
+    lateinit var testString: String
+
+    private val viewModel: TestViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Test string from MainActivity: $testString")
+    }
+}
